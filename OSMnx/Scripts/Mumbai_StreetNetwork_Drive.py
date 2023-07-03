fe4bbcec1d65e201e2199d0bb014555d57f90810
imkundankrishna <68986815+imkundankrishna@users.noreply.github.com>
2023-07-03 22:20:32 +0530

    Delete OSMnx directory

--- OSMnx/Scripts/Mumbai_StreetNetwork_Drive.py
+++
@@ -1,3 +0,0 @@
-import osmnx as ox
-a = ox.graph_from_place('Mumbai, India',network_type= "drive")
-ox.save_graph_shapefile(a, filepath="F:/Programming/osmnx/shapefiles/Mumbai_street_drive", encoding="utf-8")
